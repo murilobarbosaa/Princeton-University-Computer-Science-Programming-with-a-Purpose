9e6c20c264c6a08823b4c48911a9b63cfba07a56
murilobarbosaa <murilobarbosa2811@gmail.com>
2026-01-12 19:30:05 -0300

    feat(module-01): complete Basic Programming Concepts exercises

---
+++ 01-Basic-Programming-Concepts/CMYKtoRGB.java
@@ -0,0 +1,18 @@
+public class CMYKtoRGB {
+
+    public static void main(String[] args) {
+        double cyan = Double.parseDouble(args[0]);
+        double magenta = Double.parseDouble(args[1]);
+        double yellow = Double.parseDouble(args[2]);
+        double black = Double.parseDouble(args[3]);
+
+        double white = 1 - black;
+        double red = 255 * white * (1 - cyan);
+        double green = 255 * white * (1 - magenta);
+        double blue = 255 * white * (1 - yellow);
+
+        System.out.printf("red   = %.0f%n", red);
+        System.out.printf("green = %.0f%n", green);
+        System.out.printf("blue  = %.0f%n", blue);
+    }
+}
---
+++ 01-Basic-Programming-Concepts/HelloGoodbye.java
@@ -0,0 +1,10 @@
+public class HelloGoodbye {
+
+    public static void main(String[] args) {        
+        String nome1 = args[0];
+        String nome2 = args[1];
+
+        System.out.println("Hello " + nome1 + " and " + nome2 + ".");
+        System.out.println("Goodbye " + nome2 + " and " + nome1 + ".");
+    }
+}
